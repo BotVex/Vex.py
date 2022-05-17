b038ce9402ee967b2f2a2a88072db40f7f8162b0
Lobooooooo14 <crssociallobo@gmail.com>
2022-05-16 23:32:30 -0300

    a

--- cogs/Setavatar.py
+++ cogs/Setavatar.py
@@ -17,14 +17,14 @@ class Setavatar(commands.Cog):
 	
 	
 	@commands.slash_command(guild_ids=[957509903273046067])
-	async def setavatar(inter: disnake.ApplicationCommandInteraction, avatar: disnake.Attachment):
+	async def setavatar(self, inter: disnake.ApplicationCommandInteraction, avatar: disnake.Attachment):
 		await inter.response.defer()
 		
 		try:
 			self.bot.user.edit(avatar=requests.get(avatar.url).content)
 			await inter.send('avatar alterado!')
-		except exception as e:
-			await inter.send(f'falha ao setar o avatar:\n\n{e}')
+		except:
+			await inter.send(f'falha ao setar o avatar')
 	
 	
 def setup(bot):
