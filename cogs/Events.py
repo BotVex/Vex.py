5adbfa34138b413e7ec0c1c1af6a877d899c8847
Lobooooooo14 <crssociallobo@gmail.com>
2022-05-21 10:22:24 -0300

    a

--- cogs/Events.py
+++ cogs/Events.py
@@ -12,13 +12,13 @@ class Errors(commands.Cog):
 
 	
 	
-	@self.event
+	@self.bot.event
 	async def on_slash_command(inter: ApplicationCommandinteraction):
 		
 		print(f'Executed {inter.data.name} command in {inter.guild.name} (ID: {inter.guild.id}) by {inter.author} (ID: {inter.author.id})')
 		
 	
-	@self.event
+	@self.bot.event
 	async def on_slash_command_error(inter: ApplicationCommandinteraction, error: Exception):
 		
 		if isinstance(error, commands.CommandOnCooldown):
