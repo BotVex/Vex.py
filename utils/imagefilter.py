3a9f7a0eff260aa50f279b943bf1fa3a46964cae
Lobooooooo14 <crssociallobo@gmail.com>
2022-06-03 23:48:48 -0300

    a

--- utils/imagefilter.py
+++ utils/imagefilter.py
@@ -16,47 +16,47 @@ class Filters:
 	
 	
 	def flip(imagebytes):
-		img_obj = Image.open(BytesIO(imagebytes))
+		img_obj = Image.open(BytesIO(imagebytes)).convert('RGB')
 		return ImageOps.flip(img_obj)
 	
 	
 	def mirror(imagebytes):
-		img_obj = Image.open(BytesIO(imagebytes))
+		img_obj = Image.open(BytesIO(imagebytes)).convert('RGB')
 		return ImageOps.mirror(img_obj)
 	
 	
 	def invert(imagebytes):
-		img_obj = Image.open(BytesIO(imagebytes))
+		img_obj = Image.open(BytesIO(imagebytes)).convert('RGB')
 		return ImageOps.invert(img_obj)
 	
 	
 	def posterize(imagebytes, bits):
-		img_obj = Image.open(BytesIO(imagebytes))
+		img_obj = Image.open(BytesIO(imagebytes)).convert('RGB')
 		return ImageOps.posterize(img_obj, bits)
 	
 	
 	def solarize(imagebytes, threshold=128):
-		img_obj = Image.open(BytesIO(imagebytes))
+		img_obj = Image.open(BytesIO(imagebytes)).convert('RGB')
 		return ImageOps.solarize(img_obj, threshold=threshold)
 	
 	
 	def grayscale(imagebytes):
-		img_obj = Image.open(BytesIO(imagebytes))
+		img_obj = Image.open(BytesIO(imagebytes)).convert('RGB')
 		return ImageOps.grayscale(img_obj)
 	
 	
 	def unsharp(imagebytes, radius=2, percent=150, threshold=3):
-		img_obj = Image.open(BytesIO(imagebytes))
+		img_obj = Image.open(BytesIO(imagebytes)).convert('RGB')
 		return img_obj.filter(ImageFilter.UnsharpMask(radius=radius, percent=percent, threshold=threshold))
 	
 	
 	def gaussianblur(imagebytes, radius=2):
-		img_obj = Image.open(BytesIO(imagebytes))
+		img_obj = Image.open(BytesIO(imagebytes)).convert('RGB')
 		return img_obj.filter(ImageFilter.GaussianBlur(radius=radius))
 	
 	
 	def pixelize(imagebytes, bits=32, resize=True):
-		img_obj = Image.open(BytesIO(imagebytes))
+		img_obj = Image.open(BytesIO(imagebytes)).convert('RGB')
 		imgSmall = img_obj.resize((bits, bits),resample=Image.BILINEAR)
 		
 		if resize is True:
@@ -67,7 +67,7 @@ class Filters:
 	
 	def image2ascii(imagebytes, ascii_chars):
 		
-		image = Image.open(BytesIO(imagebytes))
+		image = Image.open(BytesIO(imagebytes)).convert('RGB')
 		
 		ASCII_CHARS = []
 		for i in ascii_chars:
