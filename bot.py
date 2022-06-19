3579536d85a796fb8a353cbd088e9582bd456b24
Lobooooooo14 <crssociallobo@gmail.com>
2022-06-19 19:31:43 -0300

    a

--- bot.py
+++ bot.py
@@ -45,8 +45,9 @@ async def on_ready():
 
 @tasks.loop(minutes=1.0)
 async def status_task():
-	shard_ids = [for x in bot.shard_count - 1]
-	print(shard_ids)
+	pass
+	#shard_ids = [for x in bot.shard_count - 1]
+	#print(shard_ids)
 		#await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.streaming, name=f'Shard: {bot.get_shard(guild.shard_id).id} | Shard latency: {int(round(bot.get_shard(guild.shard_id).latency, 2)*1000)}ms'), shard_id=guild.shard_id)
 	
 
