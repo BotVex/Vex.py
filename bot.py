83ce2e27fabde60b14c2b1d32e6cea9908450430
Lobooooo14 <crssociallobo@gmail.com>
2022-07-07 01:54:14 -0300

    removed statcord

--- bot.py
+++ bot.py
@@ -14,8 +14,6 @@ import config
 from rich.console import Console
 CO = Console()
 
-from statcord import StatcordClusterClient
-
 os.system('clear')
 
 
@@ -42,7 +40,6 @@ async def on_ready():
 	#await channel.send('online')
 
 
-
 @tasks.loop(minutes=1.0)
 async def status_task():
 	humans = []
@@ -58,10 +55,8 @@ async def status_task():
 	shard_ids = sorted(set(shard_ids))
 	
 	for shard_id in shard_ids:
-		StatcordClusterClient(bot, config.STATCORDKEY, shard_id)
 		await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.streaming, name=f'{len(bot.guilds)} Servers | {len(humans)} Users', shard_id=shard_id))
-		
-		
+	
 
 c = 0
 if __name__ == '__main__':
