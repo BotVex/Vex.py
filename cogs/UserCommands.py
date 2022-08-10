5e44ad8d07d54dc29c561a426a5c2ca67866ac21
Lobo <88998991+Lobooooooo14@users.noreply.github.com>
2022-08-10 02:22:15 -0300

    fix

--- cogs/UserCommands.py
+++ cogs/UserCommands.py
@@ -1,7 +1,8 @@
 import disnake
 from disnake.ext import commands
 EB = disnake.Embed
-ACI = disnake.ApplicationCommandInteraction
+from disnake import ApplicationCommandInteraction
+
 
 from utils.assets import Emojis as E
 from utils.assets import Colors as C
@@ -12,11 +13,10 @@ class UserCMD(commands.Cog):
 		self.bot: commands.Bot = bot 
 
 
-	@commands.user_command(name="Avatar")
-	async def avatar(inter: ACI, user: disnake.User):
-		embed = EB(title=f'Avatar de {user}')
+	@commands.user_command(name='avatar')
+	async def avatar(self, inter: disnake.ApplicationCommandInteraction, user: disnake.User):
+		embed = disnake.Embed(title=f'avatar de {user}')
 		embed.set_image(url=user.display_avatar.url)
-
 		await inter.response.send_message(embed=embed, ephemeral=True)
 
 
