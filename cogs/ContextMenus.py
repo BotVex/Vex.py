10e08a53fc6e2bb29f88e1061e9ccc8d8b12f019
Lobo <88998991+Lobooooooo14@users.noreply.github.com>
2022-08-10 12:47:42 -0300

    small anthers, renamed

--- cogs/ContextMenus.py
+++ cogs/ContextMenus.py
@@ -7,18 +7,17 @@ from utils.assets import Emojis as E
 from utils.assets import Colors as C
 
 
-class UserCMD(commands.Cog):
+class CTXCMD(commands.Cog):
 	def __init__(self, bot):
 		self.bot: commands.Bot = bot 
 
 
-	@commands.user_command(name="Avatar")
-	async def avatar(inter: disnake.ApplicationCommandInteraction, user: disnake.User):
-		embed = EB(title=f'Avatar de {user}')
+	@commands.user_command(name="avatar")
+	async def avatar(self, inter: disnake.ApplicationCommandInteraction, user: disnake.User):
+		embed = EB(title=f'avatar de {user}')
 		embed.set_image(url=user.display_avatar.url)
-
 		await inter.response.send_message(embed=embed, ephemeral=True)
 
 
 def setup(bot):
-	bot.add_cog(UserCMD(bot))
+	bot.add_cog(CTXCMD(bot))
