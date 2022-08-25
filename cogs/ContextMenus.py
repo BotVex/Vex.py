b992246797c4a23c7bb3e7e36f3a699f5af07333
Lobo <88998991+Lobooooooo14@users.noreply.github.com>
2022-08-25 08:13:46 -0300

    localized context menus

--- cogs/ContextMenus.py
+++ cogs/ContextMenus.py
@@ -1,7 +1,7 @@
 import disnake
 from disnake.ext import commands
 EB = disnake.Embed
-
+from disnake import Localized
 
 from utils.assets import Emojis as E
 from utils.assets import Colors as C
@@ -9,22 +9,21 @@ from utils.assets import Colors as C
 
 class CTXCMD(commands.Cog):
 	def __init__(self, bot):
-		self.bot: commands.Bot = bot 
+		self.bot: commands.Bot = bot
 
-
-	@commands.user_command(name="avatar")
+	@commands.user_command(name=Localized('Avatar', key='CTXMENUS_USERCMD_AVATAR_NAME'))
 	async def avatar(self, inter: disnake.ApplicationCommandInteraction, user: disnake.User):
 		embed = EB(title=f'avatar de {user}')
 		embed.set_image(url=user.display_avatar.url)
 		await inter.response.send_message(embed=embed, ephemeral=True)
 
 
-	@commands.message_command(name="ID")
+	@commands.message_command(name=Localized('ID', key='CTXMENUS_MSGCMD_ID_NAME'))
 	async def _id(self, inter: disnake.ApplicationCommandInteraction, msg: disnake.Message):
 		await inter.response.send_message(msg.id, ephemeral=True)
 	
 
-	@commands.user_command(name="Spotify", dm_permission=False)
+	@commands.user_command(name=Localized('Spotify', key='CTXMENUS_USERCMD_SPOTIFY_NAME'), dm_permission=False)
 	async def spotifysong(self, inter: disnake.CmdInter, user: disnake.Member):
 			if not any(isinstance(x, disnake.Spotify) for x in list(user.activities)):
 					await inter.send("Este usuário não está ouvindo músicas no Spotify.", ephemeral=True)
