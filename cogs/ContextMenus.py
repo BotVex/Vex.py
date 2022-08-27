2636d68b6abfe4c1df3ffc42791f556e9af2cb6f
Lobooooo14 <crssociallobo@gmail.com>
2022-08-27 20:37:10 -0300

    removed Spotify user command due to its uselessness

--- cogs/ContextMenus.py
+++ cogs/ContextMenus.py
@@ -21,39 +21,7 @@ class CTXCMD(commands.Cog):
 	@commands.message_command(name=Localized('ID', key='CTXMENUS_MSGCMD_ID_NAME'))
 	async def _id(self, inter: disnake.ApplicationCommandInteraction, msg: disnake.Message):
 		await inter.response.send_message(msg.id, ephemeral=True)
-	
 
-	@commands.user_command(name=Localized('Spotify', key='CTXMENUS_USERCMD_SPOTIFY_NAME'), dm_permission=False)
-	async def spotifysong(self, inter: disnake.CmdInter, user: disnake.Member):
-			if not any(isinstance(x, disnake.Spotify) for x in list(user.activities)):
-					await inter.send("Este usuário não está ouvindo músicas no Spotify.", ephemeral=True)
-					return
-
-			spotify = next((x for x in list(user.activities) if isinstance(x, disnake.Spotify)), None)
-
-			embed = EB(
-				title=spotify.title,
-				description=f"Por: {''.join(spotify.artists) if len(spotify.artists) == 1 else ', '.join(spotify.artists)}",
-				color=spotify.color.value
-			)
-			embed.set_image(url=spotify.album_cover_url)
-
-
-			class TrackLink(disnake.ui.View):
-				def __init__(self):
-					super().__init__()
-					self.add_item(
-						disnake.ui.Button(
-							style=disnake.ButtonStyle.link,
-							label='Link da música',
-							url=spotify.track_url,
-							emoji=E.spotify
-						)
-					)
-
-
-			await inter.send(embed=embed, view=TrackLink(), ephemeral=True)
-	
 
 def setup(bot):
 	bot.add_cog(CTXCMD(bot))
