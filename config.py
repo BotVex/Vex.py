ed2667302c119d0a0f4ff00b5cdfbb4e21cedefb
Lobo <88998991+Lobooooooo14@users.noreply.github.com>
2022-09-01 13:29:57 -0300

    remove test extencion

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ load_dotenv()
 OWNER_ID = 783120232134082580
 GUILD_ID = 939585882883772436
 
-EXTENSIONS = ['cogs.Events', 'cogs.Entertainment', 'cogs.Administration', 'cogs.Tools', 'cogs.Bot', 'cogs.ContextMenus']
+EXTENSIONS = ['cogs.Events', 'cogs.Entertainment', 'cogs.Administration', 'cogs.Bot', 'cogs.ContextMenus']
 
 TOKEN = os.environ['TOKEN']
